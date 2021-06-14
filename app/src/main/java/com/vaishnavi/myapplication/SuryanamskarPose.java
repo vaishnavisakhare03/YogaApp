2b0baa931d74321f3c30e8557cef0e92be81f12f
Vaishnavi <vaishnavisakhare0312@gmail.com>
2021-06-14 13:25:04 +0530

    INITIAL COMMIT

---
+++ app/src/main/java/com/vaishnavi/myapplication/SuryanamskarPose.java
@@ -0,0 +1,49 @@
+package com.vaishnavi.myapplication;
+
+public class SuryanamskarPose {
+
+    int image;
+    String poseName;
+    String description;
+    String uses;
+
+    public SuryanamskarPose(int image, String poseName , String description , String uses) {
+        this.image = image;
+        this.poseName = poseName;
+        this.description = description;
+        this.uses = uses;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getPoseName() {
+        return poseName;
+    }
+
+    public void setPoseName(String poseName) {
+        this.poseName = poseName;
+    }
+
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public String getUses() {
+//        return uses;
+//    }
+//
+//    public void setUses(String description) {
+//        this.uses = uses;
+//    }
+
+}
